b3ffc4157229a1895475c3a1a65f80a0b400dddc
hdias <diaslinoh@gmail.com>
2016-09-30 12:50:33 -0300

    Some improvements on the logic and localStorage,

--- SenacHorario/www/js/controllers.js
+++ SenacHorario/www/js/controllers.js
@@ -3,6 +3,7 @@ angular.module('starter.controllers', [])
 
   .controller('CursosCtrl', function ($scope, $http, $ionicLoading, CursosServ, $ionicPopup, $ionicPopover, $ionicModal) {
     $scope.userInfo = {};
+    $scope.userData = {};
     $scope.cursos;
     $scope.horarios;
     $scope.semestres = [];
@@ -26,16 +27,10 @@ angular.module('starter.controllers', [])
     });
 
     $scope.$on("$ionicView.loaded", function (event, data) {
-      if (localStorage.diasSemana && localStorage.horarios) {
-        $scope.horarios = JSON.parse(localStorage.horarios);
-        $scope.diasSemana = JSON.parse(localStorage.diasSemana);
-        $scope.flagDiasSemana = true;
-        $scope.userInfo.curso = JSON.parse(localStorage.curso);
-        $scope.userInfo.semestre = JSON.parse(localStorage.semestre);
-        // console.log($scope.diasSemana);
+      if (localStorage.userData) {
+        $scope.userData = JSON.parse(localStorage.userData);
 
       }
-      // $scope.mudaDia(JSON.parse(localStorage.getItem("curso_dia")));
     });
 
     $ionicModal.fromTemplateUrl('templates/modal-messages.html', {
@@ -83,54 +78,28 @@ angular.module('starter.controllers', [])
         $ionicLoading.hide();
       }, function (data) {
         $ionicLoading.hide();
+        alert("Ocorreram problemas para carregar os Cursos, verifique seu acesso a internet.");
         carregaCurso();
         console.log("ERROR: ", data);
       });
     }
 
-    $scope.mudaDia = function (item) {
-      $scope.flagDiasSemana = false;
-
-      $scope.diasSemana.forEach(function (el2) {
-        el2.horarios = [];
-      });
-      if (!item) return;
-      localStorage.semestre = JSON.stringify(item);
-      $scope.userInfo.semestre = item;
-      $scope.horarios.forEach(function (el1) {
-
-        $scope.diasSemana.forEach(function (el2) {
-          if (item.Semestre == el1.Semestre && el1.DiaSemana == el2.id) {
-            el2.horarios.push(el1);
-          }
-        });
-
-
-      });
-      $scope.flagDiasSemana = true;
-      localStorage.setItem("horarios", JSON.stringify($scope.horarios));
-      localStorage.setItem("diasSemana", JSON.stringify($scope.diasSemana));
-      
-      console.log($scope.diasSemana);
-
-    };
-
     $scope.filterSemestre = function (item) {
       $scope.filterSemestre = item;
     }
     $scope.carregaHorarios = function (curso) {
-      localStorage.clear();
       $scope.horarios = [];
       $scope.semestres = [];
-      $scope.userInfo = {};
+      $scope.userData = {};
 
-       $scope.diasSemana.forEach(function (el2) {
+      $scope.diasSemana.forEach(function (el2) {
         el2.horarios = [];
       });
       if (!curso) return;
 
-      localStorage.curso = JSON.stringify(curso);
-      $scope.userInfo.curso = curso;
+
+      $scope.userData.curso = curso;
+
       $ionicLoading.show({
         template: 'Carregando Horarios...'
       });
@@ -148,16 +117,38 @@ angular.module('starter.controllers', [])
 
           if (!hasIn) {
             $scope.semestres.push(el);
-            // console.log("OK");
           }
-
         });
         $ionicLoading.hide();
 
-        // console.log($scope.semestres);
       }, function (data) {
         $ionicLoading.hide();
-        // console.log(data);
       });
     }
+
+    $scope.mudaDia = function (item) {
+      $scope.flagDiasSemana = false;
+
+      $scope.diasSemana.forEach(function (el2) {
+        el2.horarios = [];
+      });
+      if (!item) return;
+      $scope.horarios.forEach(function (el1) {
+
+        $scope.diasSemana.forEach(function (el2) {
+          if (item.Semestre == el1.Semestre && el1.DiaSemana == el2.id) {
+            el2.horarios.push(el1);
+          }
+        });
+
+
+      });
+      $scope.userData.semestre = item;
+      $scope.userData.flagMessage = true;
+      $scope.userData.dias = $scope.diasSemana;
+      localStorage.userData = JSON.stringify($scope.userData);
+
+
+    };
+
   });
