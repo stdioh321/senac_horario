1b10b89a38ff81d40cb8df3e7a1b6a8b10abecc6
hdias <diaslinoh@gmail.com>
2016-10-02 19:09:44 -0300

    Professores.

--- SenacHorario/www/js/controllers.js
+++ SenacHorario/www/js/controllers.js
@@ -20,8 +20,9 @@ angular.module('starter.controllers', [])
 
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
-
+      window.open = cordova.InAppBrowser.open;
     }
+
     $scope.$on("$ionicView.afterEnter", function (event, data) {
       $scope.carregaCurso();
     });
@@ -90,7 +91,19 @@ angular.module('starter.controllers', [])
     $scope.showAlert2 = function () {
       var alertPopup = $ionicPopup.alert({
         title: 'Cursos',
-        template: '<div class="list"> <div class="item"> <h3>Desenvolvedor</h3> <h2>Henrique Lino Dias</h5> <p>diaslino.h@gmail.com</p> <h4>Polygon-Games</h4> </div> </div>'
+        template: '<div class="list"> <div class="item item-text-wrap"> <h3> <i class="icon ion-heart-broken"></i> Ooppss!! Houve um problema ao carregar os cursos, verifique seu acesso a Internet e clique no botão de recarregar localizado na barra de titulo. </h3> </div> </div>'
+      });
+    };
+    $scope.showAlert3 = function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Horarios',
+        template: '<div class="list"> <div class="item item-text-wrap"> <h3> <i class="icon ion-heart-broken"></i> Ooppss!! Houve um problema ao carregar os Horarios, verifique seu acesso a Internet e tente novamente. </h3> </div> </div>'
+      });
+    };
+    $scope.showAlert4 = function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Mensagens',
+        template: '<div class="list"> <div class="item item-text-wrap"> <h3> <i class="icon ion-heart-broken"></i> Ooppss!! Houve um problema ao carregar as Mensagens, verifique seu acesso a Internet e tente novamente. </h3> </div> </div>'
       });
     };
 
@@ -114,15 +127,19 @@ angular.module('starter.controllers', [])
       }, function (data) {
         console.log("ERRO", data);
         $ionicLoading.hide();
+        $scope.showAlert4();
       });
     }
-    $scope.carregaCurso = function() {
+    $scope.carregaCurso = function () {
       $ionicLoading.show({
         template: 'Carregando Cursos...'
       });
       CursosServ.getCursos().then(function (data) {
         $scope.cursos = data.data;
+        $scope.userData.flagCursos = false;
         $ionicLoading.hide();
+        
+        
       }, function (data) {
         $ionicLoading.hide();
         $scope.showAlert2();
@@ -170,6 +187,7 @@ angular.module('starter.controllers', [])
 
       }, function (data) {
         $ionicLoading.hide();
+        $scope.showAlert3();
       });
     }
 
@@ -190,6 +208,51 @@ angular.module('starter.controllers', [])
 
 
       });
+
+      function convertImgToDataURLviaCanvas(url, callback, outputFormat) {
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function () {
+          var canvas = document.createElement('CANVAS');
+          var ctx = canvas.getContext('2d');
+          var dataURL;
+          canvas.height = this.height;
+          canvas.width = this.width;
+          ctx.drawImage(this, 0, 0);
+          dataURL = canvas.toDataURL(outputFormat);
+          callback(dataURL);
+          canvas = null;
+        };
+        img.src = url;
+      }
+
+
+      function convertFileToDataURLviaFileReader(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function () {
+          var reader = new FileReader();
+          reader.onloadend = function () {
+            callback(reader.result);
+          }
+          reader.readAsDataURL(xhr.response);
+        };
+        xhr.open('GET', url);
+        xhr.send();
+      }
+
+      function ImageToBase64(img, mime_type) {
+        // New Canvas
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        // Draw Image
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        // To Base64
+        return canvas.toDataURL(mime_type);
+      }
+
       $scope.diasSemana.forEach(function (el1, idx1, arr1) {
         var tmpDisciplina;
         var tmpIndex;
@@ -201,14 +264,35 @@ angular.module('starter.controllers', [])
           } else {
             tmpDisciplina = el2.Disciplina;
             tmpIndex = idx2;
-            var tmpProfessor = professores.filter(function(el3){
-              if(el2.Professor == el3.name) return true; 
+            var tmpProfessor = professores.filter(function (el3) {
+              if (el2.Professor == el3.name) return true;
             });
-            if(tmpProfessor.length > 0 ) {
+            if (tmpProfessor.length > 0) {
+
+              // var image = new Image();
+              // // //image.src = 'data:image/png;base64,iVBORw0K...';
+              // image.setAttribute('crossOrigin', 'anonymous');
+              // image.src = tmpProfessor[0].photo;
+              // image.onload = function(){
+
+              //   var b64 = ImageToBase64(image, "image/png|gif|jpg");
+              //   console.log(b64);
+
+              //   // console.log(" SUCCESS", image);
+
+              // }
+
+              // function callBack(base64){
+              //   console.log(base64)
+              // }
+              // convertFileToDataURLviaFileReader(tmpProfessor[0].photo, callBack )
+              // convertImgToDataURLviaCanvas(tmpProfessor[0].photo, callBack , 'image/png|gif|jpg')
+
               el2.infoProfessor = tmpProfessor[0];
             }
-            else{
-              el2.infoProfessor = null;
+            else {
+              el2.infoProfessor = {};
+              el2.infoProfessor.photo = "img/user-default.png";
             }
             return true;
           }
@@ -226,4 +310,8 @@ angular.module('starter.controllers', [])
 
     };
 
+    $scope.openInBrowser = function (url) {
+      window.open(encodeURI(url), '_system');
+      return false;
+    }
   });
