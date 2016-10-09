5e7bfe51987b323a4ab3f765a6fa1cf615c9569b
diaslinoh@gmail.com <bhimA123>
2016-10-09 14:29:19 -0300

    Test Splash 3.

--- SenacHorario/www/js/app.js
+++ SenacHorario/www/js/app.js
@@ -8,45 +8,51 @@
 
 var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // alert("Platform Ready");
-      var splash = document.getElementById("custom-overlay");
-      setTimeout(function(){
-          splash.className = "splash-out";
-      }, 5000);
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // alert("Platform Ready");
+        var splash = document.getElementById("custom-overlay");
+        splash.className = "splash-in";
+        setTimeout(function() {
+            splash.className = "splash-out";
+            setTimeout(function() {
+                // console.log("Removed");
+                splash.parentNode.removeChild(splash);
+            }, 3000);
+
+        }, 5000);
 
 
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      // if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      //   cordova.plugins.Keyboard.disableScroll(true);
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        // if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        //   cordova.plugins.Keyboard.disableScroll(true);
 
-      // }
-      // if (window.StatusBar) {
-      //   // org.apache.cordova.statusbar required
-      //   StatusBar.styleDefault();
-      // }
+        // }
+        // if (window.StatusBar) {
+        //   // org.apache.cordova.statusbar required
+        //   StatusBar.styleDefault();
+        // }
     });
-  })
+})
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider
-      .state('cursos', {
-        url: '/cursos',
-        // abstract: true,
-        templateUrl: 'templates/cursos.html',
-        controller: "CursosCtrl"
-      });
+        .state('cursos', {
+            url: '/cursos',
+            // abstract: true,
+            templateUrl: 'templates/cursos.html',
+            controller: "CursosCtrl"
+        });
 
-      
+
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/cursos');
 
-  });
+});
